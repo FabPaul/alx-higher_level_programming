551cf41dd8cc10e522507e8a05ff63a888f20bb0
FabPaul <paulfopa0@gmail.com>
2023-05-09 08:31:43 -0700

    task6 output corrected

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(i+1, 10):
         if i == 8 and j == 9:
-            print('98')
+            print('89')
         else:
             print('{:d}{:d}, ' .format(i, j), end='')
