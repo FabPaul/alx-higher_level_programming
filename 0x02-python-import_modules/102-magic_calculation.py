16e8b78d4acd76010c2ebf0f887c610f6619c8c3
FabPaul <paulfopa0@gmail.com>
2023-05-11 14:43:24 -0700

    task8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         if a < b:
             c = add(a, b)
 
-            for i in range(4, 7):
+            for i in range(4, 6):
                 c = add(c, i)
 
             return c
