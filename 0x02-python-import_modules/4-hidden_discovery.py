47e6b23c6cef7219d059ede312f2e5a0e85c31bc
FabPaul <paulfopa0@gmail.com>
2023-05-11 09:47:38 -0700

    task4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import hidden_4
+
+    files = dir(hidden_4)
+
+    for i in files:
+        if not i.startswith('__'):
+            print(i)
