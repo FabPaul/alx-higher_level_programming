0fa80a94b2278de1f512009923f3a7e8e4ece052
FabPaul <paulfopa0@gmail.com>
2023-05-15 06:33:37 -0700

    pycodestyle

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -8,7 +8,7 @@ def divisible_by_2(my_list=[]):
         if i % 2 == 0:
             new_list.append(True)
 
-        elif i%2 != 0:
+        elif i % 2 != 0:
             new_list.append(False)
 
     return new_list
