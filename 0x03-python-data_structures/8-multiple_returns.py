388e10f30dec8ef64bae2f3ab4f2484f35516105
FabPaul <paulfopa0@gmail.com>
2023-05-15 07:21:26 -0700

    task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,9 +3,10 @@
 def multiple_returns(sentence):
 
     a = len(sentence)
-    initial_value = sentence[0]
 
     if a == 0:
         initial_value = None
+    else:
+        initial_value = sentence[0]
 
     return (a, initial_value)
