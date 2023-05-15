21fd82d332464f2763bae94c8a55fafa86e577f6
FabPaul <paulfopa0@gmail.com>
2023-05-15 17:49:28 +0000

    Still working on it

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,9 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - Function that prints basic info on python list
+ * @p: the object to fetch the info from
+ */
+
+void print_python_list_info(PyObject *p)
