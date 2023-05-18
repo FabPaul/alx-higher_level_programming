54de0e858b9c3004a62097a43dda3aef27038c1c
FabPaul <paulfopa0@gmail.com>
2023-05-18 01:38:08 -0700

    task12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    if not roman_string or type(roman_string) != str:
+        return 0
+
+    total = 0
+    old_value = 0
+
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    for value in reversed(roman_string):
+        result = roman_numerals.get(value, 0)
+
+        if result >= old_value:
+            total += result
+
+        else:
+            total -= result
+
+        old_value = result
+
+    return total
