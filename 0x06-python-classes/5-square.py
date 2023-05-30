2518d77141227ebeaef5c27029c59df579e93f41
FabPaul <paulfopa0@gmail.com>
2023-05-30 13:59:47 -0700

    task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# https://github.com/FabPaul
+""" Area of a square """
+
+
+class Square:
+    """ Definition of the class Square """
+
+    def __init__(self, size=0):
+        """ Passing of arguments into the square.
+        Args:
+            size: size of the square'
+        Raise:
+            TypeError: if size is not an interger.
+            ValueError: if type is less than zero.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """ To retrieve the size of the square """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+
+        if value < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = value
+
+    def area(self):
+        """ Function to calculate the area of the square.
+        Return: the square area
+        """
+
+        return self.__size ** 2
+
+    def my_print(self):
+        """ Print in stdout the square with the char # """
+        if self.__size == 0:
+            print()
+
+        for i in range(self.__size):
+            print('#' * self.__size)
