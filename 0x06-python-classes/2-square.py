e6c2e8fe05d19bfb64b1f6e826dfd2e8ff60eae9
FabPaul <paulfopa0@gmail.com>
2023-05-30 13:15:39 -0700

    task2 typo fixed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,9 +16,9 @@ class Square:
         """
 
         if not isinstance(size, int):
-            raise TypeError('size must be an interger')
+            raise TypeError('size must be an integer')
 
         if size < 0:
-            raise ValueError('size must be >=0')
+            raise ValueError('size must be >= 0')
 
         self.__size = size
