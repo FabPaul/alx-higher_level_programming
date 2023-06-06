d861d42c6252286976ce4d4556facc9f65dbdaab
FabPaul <paulfopa0@gmail.com>
2023-06-06 05:10:04 -0700

    A class that prevents a user from dynamically creating new instances, except it's called first_name

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+""" A module that prevents users from dynamically creating new instances """
+
+class LockedClass:
+    """ Iitializing the class """
+
+    """ __slots__ is the stopper """
+    __slots__ = ['first_name']
