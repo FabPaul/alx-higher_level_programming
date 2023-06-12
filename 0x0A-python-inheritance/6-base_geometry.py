1681f552bb29880a00d373a55289e6fa1e3f9fb4
FabPaul <paulfopa0@gmail.com>
2023-06-12 08:21:08 -0700

    A function in the BaseGeometry class that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Module for a empty class BaseGeometry """
+
+
+class BaseGeometry:
+    """ A class BaseGeometry """
+
+    def area(self):
+        """ FUnction that raises an exception message in a class """
+
+        raise Exception('area() is not implemented')
