909fc283660ec6118db019f517cec4d02a469daf
FabPaul <paulfopa0@gmail.com>
2023-06-14 02:35:36 -0700

    Python class to Json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+    Module of a function that deines a dictionare with simple data structures
+    for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """ Function that returns dict representaion for json serialization """
+
+    return 0bj.__dict__
