0aa6bcbc7b55a60b9dd049a9ea3557a50f91c328
FabPaul <paulfopa0@gmail.com>
2023-06-19 16:50:47 +0100

    tasks 11 - 13

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,8 +13,41 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
-    def __Str__(self):
+    def __str__(self):
         """ Function that returns the str representation of the Square """
 
         return ('[Square] ({}) {}/{} - {}'
-                .format(self.id, self.x, self.y, self.idwidth))
+                .format(self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        """ To retrieve the size of the sqaure """
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ To set the size the size of the square """
+
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """ Function that assigns attributes """
+
+        if args:
+            names_of_att = ['id', 'size', 'x', 'y']
+
+            for i, j in enumerate(args):
+                setattr(self, names_of_att[i], j)
+
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Function that returns the dictionary representation of Rectangle"""
+
+        dicnary = {'id': self.id, 'width': self.width, 'height': self.height,
+                   'x': self.x, 'y': self.y}
+        return dicnary
