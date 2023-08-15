91abbec10ec7334e48c47d86f83b24f02dd2fb44
FabPaul <paulfopa0@gmail.com>
2023-08-15 17:44:36 +0100

    task5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,43 +2,12 @@
 
 // Class Square that defines a square and inherits from Rectangle
 
-class Rectangle {
-  constructor (w, h) {
-    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    if (!this.width || !this.height) {
-      return; // if ther is an empty class, print nothing
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    if (!this.width || !this.height) {
-      return;
-    }
-
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super (size, size);
+    super(size, size);
   }
 }
 
-module.exports = { Rectangle, Square };
+module.exports = Square;
