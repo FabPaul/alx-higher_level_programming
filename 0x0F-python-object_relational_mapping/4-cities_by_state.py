9872c884338eeb88297a4894a230e50ada569637
FabPaul <paulfopa0@gmail.com>
2023-08-20 23:21:29 +0100

    task4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,12 +11,13 @@ if __name__ == '__main__':
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
 
     db_cursor = db.cursor()
-    db_cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    db_cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
           JOIN states ON cities.states_id = states.id ORDER BY cities.id ASC")
 
-    states = db_cursor.fetchall()
-    for state in states:
-        print(state)
+    results = db_cursor.fetchall()
+
+    for result in results:
+        print(result)
 
     db_cursor.close()
     db.close()
