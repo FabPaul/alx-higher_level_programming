0d04f7a6cc54812ae81f4e98803579150eb9ed6a
FabPaul <paulfopa0@gmail.com>
2023-08-23 19:29:39 +0100

    task7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" Script that lists all State objects from the DB hbnb_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import crate_engine
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Checks if the script is being run as the main program. If it is,
+    access the DB and extract the States
+    """
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db_string = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(db_string.format(username, password, database))
+
+    Session = sesionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query.order_by(States.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, states.name))
+
+    session.close()
