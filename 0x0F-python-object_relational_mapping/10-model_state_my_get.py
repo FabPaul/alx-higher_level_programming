0b90fcfc9480db882f7d27f46ba1ac160bdca3e7
FabPaul <paulfopa0@gmail.com>
2023-08-23 20:08:30 +0100

    task10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+""" Script that prints State objects with name arg from the DB hbnb_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Checks if the script is being run as the main program. If it is,
+    access the DB and extract the States
+    """
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db_string = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(db_string.format(username, password, database))
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State).filter(State.name == argv[4]).first()
+
+    if states is not None:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+
+    session.close()
