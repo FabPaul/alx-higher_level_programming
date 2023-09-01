b5cfa54aa6b0c417ab403c421f88c819bc0d7818
FabPaul <paulfopa0@gmail.com>
2023-09-01 22:28:58 +0100

    task9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ import requests
 from sys import argv
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     url = "https://api.github.com/user"
     auth = (argv[1], argv[2])
     req = requests.get(url, auth=auth).json()
